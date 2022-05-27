cb77669b7e2fc7c391f4a24052aae8931a65b193
shaochenheng <1010716589@qq.com>
2022-05-27 23:57:36 +0000

    Try to use node.js NeteaseCloudMusicApi (just a test may be have some problem of npm).

--- src/song.js
+++ src/song.js
@@ -1,64 +1,61 @@
-import axios from 'axios';
-
-const HOST = 'https://netease-cloud-music-api-teal-tau.vercel.app';
-
-axios.defaults.withCredentials = true;
-
-const API = {};
-API.LYRIC = '/lyric';
-API.SEARCH = '/search';
-API.SONG = '/song/url';
-
 let Search = {};
 
-Search.search = function(keyword, limit, callback) {
-  let url = HOST + API.SEARCH + '?keywords=' + keyword + '?limit=' + limit + '?type=1';
+const {
+  song_url,
+  lyric,
+  search,
+} = require('NeteaseCloudMusicApi');
 
-  axios.get(url)
-  .then((response) => {
-    let data = response.data;
-    if (data.code == 200) {
-      callback && callback(data);
-    }
-  })
-  .catch((error) => {
-    console.log(error);
-    callback && callback([]);
-  });
+Search.searchSong = async function(keyword, limit, callback) {
+  try {
+    let result = await search({
+      type: '1',
+      limit: limit,
+      keywords: keyword
+    }).then((response) =>{
+      let data = response.body;
+      if (data.code == 200) {
+        callback && callback(data);
+      }
+    });
+  } catch (error) {
+      console.log(error);
+      callback && callback([]);
+  }
 };
 
 Search.getSongId = function(song, callback) {
   let keyword = song.name + ' ' + song.artist.split('/')[0];
 
-  Search.search(keyword, '100', (data) => {
+  searchSong(keyword, '100', (data) => {
     let songs = data.result.songs;
+    
     var sameSong = songs.filter((item) => {
       return item.name === song.name &&
              item.artists[0].name === song.artist.split('/')[0];
     });
     if (sameSong.legth != 0) {
+      console.log(sameSong.length);
       callback(sameSong);
     }
   });
 };
 
-Search.getLyric = function(song, callback) {
-  Search.getSongId(song, (data) => {
+Search.getLyric = async function(song, callback) {
+  getSongId(song, async (data) => {
     let songId = data[0].id;
-    let url = HOST + API.LYRIC + '?id=' + songId;
-
-    axios.get(url)
-    .then((response) => {
-      let data = response.data;
-      if (data.code == 200) {
-        let lyric = data.lrc.lyric;
-        callback && callback(lyric);
-      }
-    })
-    .catch((error) => {
-      console.log(error);
+    try {
+      let result = await lyric({
+        id: songId,
+      })
+      .then((response) => {
+        callback && callback(response);
+        console.log(response);
+      });
+    } catch (error) {
       callback && callback([]);
-    });
+      console.log(error);
+    }
   });
 };
 
