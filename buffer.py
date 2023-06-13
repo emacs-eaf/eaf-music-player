29c9b4bf1c174c45ae4a2cf7e820b0ecd5596e2d
Andy Stewart <lazycat.manatee@gmail.com>
2023-06-13 19:45:43 +0800

    Add QQMusicLyric

--- buffer.py
+++ buffer.py
@@ -30,7 +30,11 @@ import os
 import mimetypes
 import taglib
 import subprocess
-import threading
+import time
+import base64
+import requests
+from urllib.parse import quote_plus
+from typing import Optional
 
 class AppBuffer(BrowserBuffer):
     def __init__(self, buffer_id, url, arguments):
@@ -156,8 +160,6 @@ class AppBuffer(BrowserBuffer):
 
     def update_lyric(self, track, lyric):
         # Only update cover when
-        print("****** ", track, lyric)
-
         if track == self.vue_current_track:
             self.buffer_widget.eval_js_function("updateLyric", string_to_base64(lyric))
 
@@ -307,18 +309,85 @@ class FetchLyric(QThread):
         self.artist = artist
         self.album = album
 
-    def run(self):
+    def get_lyric_from_netease(self):
         node_process = subprocess.Popen(['node', self.lyric_js, self.title, self.artist, self.album],
                                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=False)
         output, _ = node_process.communicate()
 
-        result = output.decode('utf-8')
+        return output.decode('utf-8')
+
+    def get_lyric_from_qq_music(self):
+        try:
+            api = QQMusicLyric()
+            return api.query_lyric(self.title, self.artist).decode("utf-8")
+        except:
+            return None
+
+    def run(self):
+        result = self.get_lyric_from_qq_music()
+        if result is None:
+            result = self.get_lyric_from_netease()
 
         lyric_path = get_lyric_path(self.lyrics_cache_dir, self.artist, self.title)
         with open(lyric_path, "w") as f:
             f.write(result)
 
         self.fetch_result.emit(self.track, result)
+
+class QQMusicLyric:
+
+    def query_lyric(self, name: str, artist: Optional[str] = "") -> Optional[str]:
+        mid = self._search_song(name, artist)
+        if not mid:
+            return None
+        return self._download_lyric(mid)
+
+    def _search_song(self, name: str, artist: Optional[str] = "") -> Optional[str]:
+        key = quote_plus(f"{name} {artist}".strip())
+        url = "https://c.y.qq.com/splcloud/fcgi-bin/smartbox_new.fcg?format=json" \
+              "&inCharset=utf-8&outCharset=utf-8&key=" + key
+        try:
+            result = requests.get(url, headers={"Referer": "https://c.y.qq.com/"}).json()
+        except Exception as e:
+            print(f"query name: {name}, artist: {artist} lyric fail: {e}")
+            result = {}
+        item_list = result.get("data", {}).get("song", {}).get("itemlist", None)
+        if not item_list:
+            return None
+        return item_list[0].get("mid", None)
+
+    def _download_lyric(self, mid: str) -> Optional[str]:
+        if not mid:
+            return None
+        current_millis = int((time.time()) * 1000)
+        data = {
+            'pcachetime': str(current_millis),
+            'songmid': mid,
+            'g_tk': '5381',
+            'loginUin': '0',
+            'hostUin': '0',
+            'format': 'json',
+            'inCharset': 'utf8',
+            'outCharset': 'utf8',
+            'notice': '0',
+            'platform': 'yqq',
+            'needNewCode': '0',
+        }
+
+        url = 'https://c.y.qq.com/lyric/fcgi-bin/fcg_query_lyric_new.fcg'
+        headers = {
+                'Referer': 'https://c.y.qq.com/'
+        }
+        try:
+            result = requests.post(url, data=data, headers=headers).json()
+        except Exception as e:
+            print(f"[qqmusic lyric] download mid: {mid} fail: {e}")
+            result = {}
+
+        lyric = result.get("lyric", None)
+        if not lyric:
+            return None
+        return base64.b64decode(lyric).decode("utf-8")
 
 def get_lyric_path(lyrics_cache_dir, artist, title):
     return os.path.join(lyrics_cache_dir, "{}_{}.lyc".format(artist.replace("/", "_"), title.replace("/", "_")))
